f574ce1d8afde53e5d1992f6ba705c461aac09be
Himcd <mc_himcd@163.com>
2024-04-19 23:01:15 +0800

    240419

--- src/main/java/himcd/heretic/TickRunner.java
+++ src/main/java/himcd/heretic/TickRunner.java
@@ -30,16 +30,12 @@ public final class TickRunner extends BukkitRunnable {
         if (chooseMenu != null) chooseMenu.tick();
         //信息显示
         switch (state) {
-            case NONE -> {
-                var ps = Bukkit.getOnlinePlayers().stream()
-                        .filter(p -> p.getGameMode() == GameMode.ADVENTURE && !MainMenu.prepared.contains(p));
-                beforeStart(msg.deserialize("%s %s".formatted(
-                        prepareTime == -1 ? "人数过少" : "距离游戏开始：%s".formatted((prepareTime + 20) / 20),
-                        ps.findAny().isPresent() ?
-                                "未准备玩家：" + ps.map(Player::getName).collect(Collectors.joining(","))
-                                : ""
-                )));
-            }
+            case NONE -> beforeStart(msg.deserialize("%s %s".formatted(
+                    prepareTime == -1 ? "人数过少" : "距离游戏开始：%s".formatted((prepareTime + 20) / 20),
+                    Bukkit.getOnlinePlayers().stream().anyMatch(p -> p.getGameMode() == GameMode.ADVENTURE && !MainMenu.prepared.contains(p)) ?
+                            "未准备玩家：" + Bukkit.getOnlinePlayers().stream().filter(p -> p.getGameMode() == GameMode.ADVENTURE && !MainMenu.prepared.contains(p)).map(Player::getName).collect(Collectors.joining(","))
+                            : ""
+            )));
             case FIRST, SECOND -> {
                 //BossBar显示
                 if (state == State.FIRST) {
--- src/main/java/himcd/heretic/game/HPlayer.java
+++ src/main/java/himcd/heretic/game/HPlayer.java
@@ -9,7 +9,9 @@ import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.scoreboard.Team;
 
@@ -69,11 +71,8 @@ public record HPlayer(Player player, Role role, Skill skill, Power power) {
             inv.addItem(power.getItem(),ItemCreator.create(Material.END_PORTAL_FRAME).amount(5).getItem());
         }
         inv.addItem(new ItemStack(Material.BREAD, 6));
-//        for (var m : new Material[]{Material.IRON_AXE, Material.IRON_PICKAXE, Material.IRON_SHOVEL}) {
-//            inv.addItem(ItemCreator.create(m).getItem());
-//        }
-        for (var m : new Material[]{Material.IRON_AXE}) {
-            inv.addItem(ItemCreator.create(m).getItem());
+        for (var m : new Material[]{Material.IRON_AXE, Material.IRON_PICKAXE, Material.IRON_SHOVEL}) {
+            inv.addItem(ItemCreator.create(m).attribute(Attribute.GENERIC_ATTACK_DAMAGE,0, EquipmentSlot.HAND).getItem());
         }
     }
 }
